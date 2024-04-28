74da6bec4a724c3311e24ede39b743b4fb672f10
sinanoslu <168214972+sinanoslu@users.noreply.github.com>
2024-04-28 15:33:30 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link :to="{name: 'about'}">About</router-link> |
+    <router-link :to="{name: 'jobs'}">Jobs</router-link>
+  </nav>
+
+  <button @click="Redirect">Redirect</button>
+  <button @click="Back">Go Back</button>
+  <button @click="Forward">Go Forward</button>
+
+  <router-view/>
+</template>
+
+<script>
+export default {
+    methods:{
+      Redirect(){
+        this.$router.push({name:'home'})
+      },
+      Back(){
+        this.$router.go(-1)
+      },
+      Forward(){
+        this.$router.go(1)
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
+}
+
+button{
+    margin: 0 10px;
+    padding: 16px;
+    border: none;
+    border-radius: 4px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia officiis consequuntur itaque qui ipsam facilis accusantium molestiae dicta inventore quis sequi dolorem deleniti quasi praesentium a sed, tempora consectetur vel aliquam odio architecto! Ex ratione eius dolores. Numquam, consequuntur? Molestias odit ipsum nostrum temporibus esse vel error quam eveniet labore, deserunt quod quae natus corporis, id consequuntur dolores dolore nam aut veniam, quis sapiente ducimus excepturi cupiditate! Deleniti, maiores, perferendis sint dolor ullam, nostrum accusantium sunt nobis vero earum praesentium cumque deserunt exercitationem eum pariatur! Amet facilis ab eaque, pariatur quae dolore quis at ad, ullam, consectetur sint exercitationem ratione?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia officiis consequuntur itaque qui ipsam facilis accusantium molestiae dicta inventore quis sequi dolorem deleniti quasi praesentium a sed, tempora consectetur vel aliquam odio architecto! Ex ratione eius dolores. Numquam, consequuntur? Molestias odit ipsum nostrum temporibus esse vel error quam eveniet labore, deserunt quod quae natus corporis, id consequuntur dolores dolore nam aut veniam, quis sapiente ducimus excepturi cupiditate! Deleniti, maiores, perferendis sint dolor ullam, nostrum accusantium sunt nobis vero earum praesentium cumque deserunt exercitationem eum pariatur! Amet facilis ab eaque, pariatur quae dolore quis at ad, ullam, consectetur sint exercitationem ratione?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
